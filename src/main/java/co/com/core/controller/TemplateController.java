03d5d1214f3ea694e53156c62ff4d48f906d55fb
Diego Nieto <Diego.Nieto@prodigious.com>
2016-03-04 12:09:03 -0500

    file upload version 1.0

--- src/main/java/co/com/core/controller/TemplateController.java
+++ src/main/java/co/com/core/controller/TemplateController.java
@@ -1,10 +1,17 @@
 package co.com.core.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
 import org.apache.log4j.Logger;
+import org.primefaces.event.FileUploadEvent;
 
 import co.com.core.commons.converter.UserUtil;
 import co.com.core.domain.User;
@@ -76,7 +83,57 @@ public class TemplateController {
 		return pageDto;
 	}
 	
+	private File file;
+	 
+    public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public void foo() {
+    	System.out.println("shut!!");
+    }
+    
+    public void upload(FileUploadEvent event) {  
+    	FacesMessage msg = new FacesMessage("Success! ", event.getFile().getFileName() + " is uploaded.");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        // Do what you want with the file        
+        try {
+            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+        } catch (IOException e) {
+            logger.error("Exception creating file: " + e.getMessage());
+        }
+    }  
+ 
+    public void copyFile(String fileName, InputStream in) {
+    	try {
+	        // write the inputStream to a FileOutputStream
+	        OutputStream out = new FileOutputStream(new File("/Users/dienieto/Documents/DocumentsDiego/projects/friogan/images/upload/" + fileName));
+	        int read = 0;
+	        byte[] bytes = new byte[1024];
+	        while ((read = in.read(bytes)) != -1) {
+	            out.write(bytes, 0, read);
+	        }
+	        in.close();
+	        out.flush();
+	        out.close();
+	        System.out.println("New file created!");
+        } catch (IOException e) {
+        	System.out.println(e.getMessage());
+        }
+    }
 	
+    
+    public void handleFileUpload(FileUploadEvent event) {
+    	System.out.println("upload file");
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+    
 	public IMenuService getMenuService() {
 		return menuService;
 	}
